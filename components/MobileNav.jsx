aca445cf804f2a8080be0797e3a901e83bb13f58
Asanka Wickramasurendra <asankadilshanme@gmail.com>
2024-12-28 16:05:21 +0530

    finished desktop and mobile nav

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -3,31 +3,50 @@
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
-import {CiMenuFries} from "react-icons/ci";
+import { CiMenuFries } from "react-icons/ci";
 
 const links = [
-    { href: "/", label: "Home" },
-    { href: "/services", label: "services" },
-    { href: "/resume", label: "resume" },
-    { href: "/contact", label: "contact" },
-    { href: "/portfolio", label: "portfolio" },
-  ];
+  { href: "/", label: "Home" },
+  { href: "/services", label: "services" },
+  { href: "/resume", label: "resume" },
+  { href: "/contact", label: "contact" },
+  { href: "/portfolio", label: "portfolio" },
+];
 
 function MobileNav() {
-    const pathname = usePathname();
+  const pathname = usePathname();
   return (
     <Sheet>
-        <SheetTrigger className="flex justify-center items-center">
-            <CiMenuFries className="text-[32px] text-accent"></CiMenuFries>
-        </SheetTrigger>
-        <SheetContent className="flex flex-col">
-            {/* logo */}
-            <div>
-                
-            </div>
-        </SheetContent>
+      <SheetTrigger className="flex justify-center items-center">
+        <CiMenuFries className="text-[32px] text-accent"></CiMenuFries>
+      </SheetTrigger>
+      <SheetContent className="flex flex-col">
+        {/* logo */}
+        <div className="mt-20 mb-40 text-center text-2xl">
+          <Link href="/">
+            <h1 className="text-4xl font-semibold">
+              Asanka<span className="text-accent">.</span>
+            </h1>
+          </Link>
+        </div>
+        {/* nav links */}
+        <nav className="flex flex-col items-center space-y-4">
+          {links.map((link, index) => (
+            <Link
+              href={link.href}
+              key={index}
+              className={`${
+                link.href === pathname &&
+                "text-accent border-b-2 border-accent"
+              } text-xl capitalize hover:text-accent transition-all`}
+            >
+              {link.label}
+            </Link>
+          ))}
+        </nav>
+      </SheetContent>
     </Sheet>
-  )
+  );
 }
 
-export default MobileNav
+export default MobileNav;
